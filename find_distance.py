80fed72c905ebb1a94ac84ec4db9e7005b38492a
Honnery <36925962+Honnery@users.noreply.github.com>
2020-01-19 11:59:57 +0300

    Reading data to excel file

---
+++ find_distance.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import pandas as pd
+# xlrd
+
+key = 'AIzaSyABvBEFYZPUNzdLENdZg8Jwb_O61XCc_-I'
+def make_list_of_plases():
+    with open('list_of_hospitals.json') as f:
+        list_of_h = json.load(f)
+    return  ['{},{}'.format( hospital['lat'], hospital['lng']) for hospital in list_of_h]
+
+
+
+def find_nearest_hospital(data):
+    value = int(data['rows'][0]['elements'][0]['duration']['value'])
+    result = {'hospital': data['destination_addresses'][0],
+            'distance': data['rows'][0]['elements'][0]['distance'],
+            'duration': data['rows'][0]['elements'][0]['duration']}
+    for hospital, info in zip(data['destination_addresses'], data['rows'][0]['elements']):
+        if int(info['duration']['value']) < value:
+            value = int(info['duration']['value'])
+            result = {'hospital': hospital,
+                    'distance': info['distance'],
+                    'duration': info['duration']}
+    return result
+
+
+
+# вот тут можно подкрутить параметры для настройки
+body = {
+    'key': key,
+    'origins': 'Maskavas iela 273',
+    'units': 'imperial',
+    'destinations': '|'.join(make_list_of_plases())
+}
+
+#от этого числа зависит сколько запросов будет сделано за один скрипт,не забудь прочекать лимиты для текущего ключа!!!!!!!!!
+number_of_addresses_for_requests = 1
+
+
+
+
+df = pd.read_excel('Adresa_kvartir.xlsx').fillna('')
+undefined_data = df.index[df['Distance Text'] == ''].tolist()
+
+for row in range(number_of_addresses_for_requests):
+    body['origins']=df['Address'][undefined_data[row]]
+    response = requests.get(
+        url='https://maps.googleapis.com/maps/api/distancematrix/json', params=body)
+    data = json.loads(response.content.decode())
+    hospital = find_nearest_hospital(data)
+    df['Distance Text'][undefined_data[row]] = hospital['distance']['text']
+    df['Distance Value'][undefined_data[row]] = hospital['distance']['value']
+    df['Duration Text'][undefined_data[row]] = hospital['duration']['text']
+    df['Duration Value'][undefined_data[row]] = hospital['duration']['value']
+
+df.to_excel('Adresa_kvartir.xlsx', index = False)
---
+++ load_all_hospitals.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+key = 'AIzaSyABvBEFYZPUNzdLENdZg8Jwb_O61XCc_-I'
+
+# вот тут исчет все больницы в окрестностях риги б можно поменять тип, и еще вставить всякие штуки типа поиска по ключевому слову
+body = {'key': key,
+        'location': '56.950000, 24.110000',
+        'radius': '15000',
+        'type': 'hospital'}
+list_of_all_hospitals = []
+
+
+def store_all_data(results):
+    for address in results:
+        location = {
+
+            'lat': address['geometry']['location']['lat'],
+            'lng': address['geometry']['location']['lng'],
+            'name': address['name']
+
+                }
+        list_of_all_hospitals.append(location)
+
+
+while True:
+    response = requests.get(
+        url='https://maps.googleapis.com/maps/api/place/nearbysearch/json', params=body)
+
+    data = json.loads(response.content.decode())
+    store_all_data(data['results'])
+
+    next_page = data.get('next_page_token')
+    if next_page:
+        body['pagetoken']= next_page
+    else:
+        break
+
+with open('list_of_hospitals_test.json', 'w') as f:
+  f.write(json.dumps(list_of_all_hospitals))
+
